8ac4bf4d19112044dc1b4a4828f193c2832e250b
Timofei <timofeimih@gmail.com>
2014-02-25 19:54:32 +0200

    class add

--- extended_flatpages/admin.py
+++ extended_flatpages/admin.py
@@ -11,19 +11,22 @@ from django.conf import settings
 from django import forms
 from ckeditor.widgets import CKEditorWidget
 from django.utils.translation import ugettext_lazy as _
+from themes.models import Theme
 
 class CustomFlatpageForm(FlatpageForm):
     locals()['content'] = forms.CharField(widget=CKEditorWidget(), required=False, label=_(u'Content'))
     description = forms.CharField(required=False, label=_(u'Description'))
     keywords = forms.CharField(required=False, label=_(u'Keywords'))
+    class_name = forms.ModelChoiceField(queryset=Theme.objects.all(), label=_(u'Class'))
     
     class Meta:
         model = CMSFlatPage
 
+
 class CustomFlatPageAdmin(FlatPageAdmin): 
     form = CustomFlatpageForm
     fieldsets = (
-        (None, {'fields': ('url', 'title', 'content', 'sites', 'keywords', 'description')}),
+        (None, {'fields': ('url', 'title', 'content', 'sites', 'keywords', 'description', 'class_name')}),
         (_('Advanced options'), {'classes': ('collapse',), 'fields': ('enable_comments', 'registration_required', 'template_name')}),
     )
     class Media:
--- extended_flatpages/models.py
+++ extended_flatpages/models.py
@@ -2,7 +2,13 @@ from django.contrib.flatpages.models import FlatPage
 from django.db import models
 from django.contrib.sites.models import Site
 from django.utils.translation import ugettext_lazy as _
+from themes.models import Theme
 
 class CMSFlatPage(FlatPage):
     description = models.CharField(verbose_name=_(u"Description"),max_length=100)
     keywords = models.CharField(verbose_name=_(u"Keywords"),max_length=255)
+    class_name = models.ForeignKey(Theme)
+
+    class Meta:
+        verbose_name = _(u'Static page')
+        verbose_name_plural = _(u'Static pages')
